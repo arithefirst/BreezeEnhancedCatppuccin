88c978bdf773bbd4c2db1c46bef6ffb2e1c9ab06
AriTheFirst <ari@arithefirst.com>
2024-05-21 21:43:59 -0400

    Added Latte Version

---
+++ Latte/build/config/CMakeFiles/kcm_breezeenhanced.dir/kcm_breezeenhanced_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+config/CMakeFiles/kcm_breezeenhanced.dir/kcm_breezeenhanced_autogen/mocs_compilation.cpp.o: \
+ /home/ari/Desktop/BreezeEnhancedCatppuccin/Mocha/build/config/kcm_breezeenhanced_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
